d68fd4427bad9b35e9d744d7c4f25c625ecf0118
Sastry Akella <sastry@akella.com>
2017-04-09 09:26:13 -0400

    Converted App from functional to Class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar'
 
 const API_KEY_YOUTUBE = 'AIzaSyCT7J9JqXAJhO864KP2UeVu1FwfGpFIpEI';
 
-YTSearch({key: API_KEY_YOUTUBE, term: 'Surfboards'}, data => {
-    console.log(data);
-});
 
-// Create a component that generates the HTML code
-const App = () => (<div><SearchBar /></div>);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY_YOUTUBE, term: 'Surfboards'}, videos => {
+        
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (<div><SearchBar /></div>);
+    }
+}
 
 // Present the Generated HTML on the view
 ReactDOM.render(<App />, document.querySelector(".container"));
